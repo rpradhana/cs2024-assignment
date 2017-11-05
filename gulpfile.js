badd820dab453c85461ce0dda9b2163336810ca8
rmonap <raymond.pradhana@gmail.com>
2017-11-05 18:27:15 +0800

    add basic homepage

--- gulpfile.js
+++ gulpfile.js
@@ -1,13 +1,18 @@
 'use strict';
 
-var gulp   = require('gulp');
-var sass   = require('gulp-sass');
-var rename = require('gulp-rename');
+var gulp           = require('gulp');
+var sass           = require('gulp-sass');
+var rename         = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
 
 gulp.task('sass', function () {
-	return gulp.src('./src/bulma/sass/style.sass')
+	return gulp.src('./src/bulma/bulma.sass')
 		.pipe(sass().on('error', sass.logError))
-		.pipe(rename('style.min.css'))
+		.pipe(rename('style.css'))
+		.pipe(autoprefixer({
+			browsers: ['last 2 versions'],
+			cascade: false
+		}))
 		.pipe(gulp.dest('./public/static/css'));
 });
 

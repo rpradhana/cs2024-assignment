431fc245f61220b3adfe3aa2bcaa02cd164638b0
rmonap <raymond.pradhana@gmail.com>
2017-11-15 03:40:39 +0800

    add index.html add services.html add privacy-policy.html add terms-conditions.html add redirects

--- public/static/js/main.js
+++ public/static/js/main.js
@@ -13,11 +13,46 @@ var ready = function(fn) {
 }
 
 ready(function() {
-	attachClassToggle($('.dropdown'), 'is-active');
-	attachClassToggle($('.navbar-burger'), 'is-active');
+	attachClassToggle($('.dropdown'), 'is-active', 'click');
+	attachClassToggle($('.navbar-burger'), 'is-active', 'click');
+	// attachVideoControlToggle($('#testimonial1'));
+	// attachVideoControlToggle($('#testimonial2'));
+	// attachVideoControlToggle($('#testimonial3'));
+	attachVideoPlayer('#testimonial1');
+	attachVideoPlayer('#testimonial2');
+	attachVideoPlayer('#testimonial3');
 });
 
-var attachClassToggle = function(selector, className) {
+
+var attachVideoPlayer = function(selector) {
+	$(selector).click(
+		function() {
+			$(selector)[0].play();
+			$(selector).attr('controls', '');
+			$(selector + ' + .video-overlay').css('display', 'none');
+			console.log($(selector + ' + .video-overlay'));
+			if ($(selector)[0].requestFullscreen) {
+				$(selector)[0].requestFullscreen();
+			} else if ($(selector)[0].mozRequestFullScreen) {
+				$(selector)[0].mozRequestFullScreen();
+			} else if (selector[0].webkitRequestFullscreen) {
+				$(selector)[0].webkitRequestFullscreen();
+			}
+		}
+	)
+}
+
+var attachVideoControlToggle = function(selector) {
+	selector.hover(
+		function() {
+			selector.attr('controls', '');
+		}, function() {
+			selector.attr('controls', null);
+		}
+	)
+}
+
+var attachClassToggle = function(selector, className, event) {
 	var $selector = Array.prototype.slice.call(selector, 0);
 
 	// Check if there are any target
@@ -25,7 +60,7 @@ var attachClassToggle = function(selector, className) {
 
 		// Add a click event on each of them
 		$selector.forEach(function ($el) {
-			$el.addEventListener('click', function () {
+			$el.addEventListener(event, function () {
 
 				// Get the target from the "data-target" attribute
 				var target = $el.dataset.target;
@@ -49,11 +84,11 @@ var scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 window.onscroll = function() {
 	scrollTop = window.pageYOffset || document.documentElement.scrollTop;
 	if (scrollTop > 0) {
-		console.log('scrollTop > 0');
+		// console.log('scrollTop > 0');
 		document.querySelectorAll('.navbar')[0].classList.remove('shadow-is-hidden');
 	}
 	else if (scrollTop == 0) {
-		console.log('scrollTop == 0');
+		// console.log('scrollTop == 0');
 		document.querySelectorAll('.navbar')[0].classList.add('shadow-is-hidden');
 	}
 }

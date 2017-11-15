30dc1dbbee04b3c556e6f23c027cf62d08acdf1f
rmonap <raymond.pradhana@gmail.com>
2017-11-15 12:49:36 +0800

    fix mobile responsiveness

--- public/static/js/services.js
+++ public/static/js/services.js
@@ -3,7 +3,7 @@ var remScale = 16;
 $(function() {
 	var draggable      = $('.sub-category__tabs.is-draggable');
 	var draggableWidth = parseInt(draggable.width(), 10);
-	var containerWidth = parseInt($('.sub-category__container').width(), 10);
+	var containerWidth = parseInt($(window).width(), 10) - 2 * 24; // minus padding 1.5rem
 
 	// Set nav to be draggable
 	draggable.draggable({
@@ -14,12 +14,14 @@ $(function() {
 
 	draggable.on('drag', function(event, ui) {
 		console.log('DRAG!');
+		console.log(containerWidth + '  ');
+		console.log(draggableWidth);
 		// Clamp scroll position within container edges
 		if(ui.offset.left > 0) {
 			ui.position.left = 0;
 		}
-		if (ui.position.left < -draggableWidth + containerWidth) {
-			ui.position.left = -draggableWidth + containerWidth;
+		if (ui.position.left < containerWidth - draggableWidth) {
+			ui.position.left = containerWidth - draggableWidth;
 		}
 	});
 });
